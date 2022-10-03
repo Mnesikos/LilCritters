1488be64aa175b6cd873f24de0c5512a270a114e
Mnesikos <mnesikos@gmail.com>
2022-10-03 17:14:14 -0400

    be less lazy lol

--- src/main/java/com/github/mnesikos/lilcritters/LilCritters.java
+++ src/main/java/com/github/mnesikos/lilcritters/LilCritters.java
@@ -1,9 +1,12 @@
 package com.github.mnesikos.lilcritters;
 
 import com.github.mnesikos.lilcritters.entity.LCEntities;
+import com.github.mnesikos.lilcritters.item.LCItems;
 import com.github.mnesikos.lilcritters.sounds.LCSounds;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -14,12 +17,24 @@ public class LilCritters {
     public LilCritters() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        LCEntities.REGISTRAR.register(bus);
+        LCItems.REGISTRAR.register(bus);
         LCSounds.REGISTRAR.register(bus);
 
         bus.addListener(this::setup);
+        bus.addListener(this::registerAttributes);
+        bus.addListener(this::setupClient);
+    }
+
+    private void registerAttributes(final EntityAttributeCreationEvent event) {
+        LCEntities.registerAttributes((type, builder) -> event.put(type, builder.build()));
     }
 
     private void setup(final FMLCommonSetupEvent event) {
         LCEntities.registerSpawnPlacements();
+    }
+
+    private void setupClient(final FMLClientSetupEvent event) {
+        LCEntities.registerRenderers();
     }
 }
---
+++ src/main/java/com/github/mnesikos/lilcritters/item/LCItems.java
@@ -0,0 +1,10 @@
+package com.github.mnesikos.lilcritters.item;
+
+import com.github.mnesikos.lilcritters.LilCritters;
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class LCItems {
+    public static final DeferredRegister<Item> REGISTRAR = DeferredRegister.create(ForgeRegistries.ITEMS, LilCritters.MOD_ID);
+}
