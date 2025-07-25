c027ea697e56535ee88ee7fbe90674f3953764bf
Mnesikos <mnesikos@gmail.com>
2025-07-24 23:42:00 -0400

    start entity loot table datagen

--- src/main/java/com/github/mnesikos/lilcritters/LilCritters.java
+++ src/main/java/com/github/mnesikos/lilcritters/LilCritters.java
@@ -1,13 +1,22 @@
 package com.github.mnesikos.lilcritters;
 
 import com.github.mnesikos.lilcritters.client.model.LCModelLayers;
+import com.github.mnesikos.lilcritters.data.LCBlockLoot;
+import com.github.mnesikos.lilcritters.data.LCEntityLoot;
+import com.github.mnesikos.lilcritters.data.LCRecipeProvider;
+import com.github.mnesikos.lilcritters.data.LCTagsProviders;
 import com.github.mnesikos.lilcritters.entity.LCEntities;
 import com.github.mnesikos.lilcritters.item.LCItems;
 import com.github.mnesikos.lilcritters.sounds.LCSounds;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.util.Tuple;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -16,6 +25,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.zawamod.zawa.Zawa;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Supplier;
 
 @Mod(LilCritters.MOD_ID)
@@ -31,6 +42,7 @@ public class LilCritters {
 
         bus.addListener(this::addCreativeTabs);
         bus.addListener(this::setup);
+        bus.addListener(this::gatherData);
         bus.addListener(this::registerLayerDefinitions);
         bus.addListener(this::setupClient);
     }
@@ -46,6 +58,18 @@ public class LilCritters {
 
     private void setup(final FMLCommonSetupEvent event) {
         LCEntities.registerSpawnPlacements();
+    }
+
+    private void gatherData(final GatherDataEvent event) {
+        DataGenerator dataGenerator = event.getGenerator();
+        PackOutput packOutput = dataGenerator.getPackOutput();
+//        LCTagsProviders.LCBlockTagsProvider blockTagsProvider = new LCTagsProviders.LCBlockTagsProvider(packOutput, event.getLookupProvider(), event.getExistingFileHelper());
+//        dataGenerator.addProvider(event.includeServer(), blockTagsProvider);
+//        dataGenerator.addProvider(event.includeServer(), new LCTagsProviders.LCEntityTypeTagsProvider(packOutput, event.getLookupProvider(), event.getExistingFileHelper()));
+        dataGenerator.addProvider(event.includeServer(), new LootTableProvider(packOutput, Collections.emptySet(), List.of(
+//                new LootTableProvider.SubProviderEntry(LCBlockLoot::new, LootContextParamSets.BLOCK),
+                new LootTableProvider.SubProviderEntry(LCEntityLoot::new, LootContextParamSets.ENTITY))));
+//        dataGenerator.addProvider(event.includeServer(), new LCRecipeProvider(packOutput));
     }
 
     private void setupClient(final FMLClientSetupEvent event) {
---
+++ src/main/java/com/github/mnesikos/lilcritters/data/LCEntityLoot.java
@@ -0,0 +1,81 @@
+package com.github.mnesikos.lilcritters.data;
+
+import com.github.mnesikos.lilcritters.LilCritters;
+import com.github.mnesikos.lilcritters.entity.LCEntities;
+import com.github.mnesikos.lilcritters.item.LCItems;
+import net.minecraft.data.loot.packs.VanillaEntityLoot;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
+import net.minecraft.world.level.storage.loot.functions.LootingEnchantFunction;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.functions.SmeltItemFunction;
+import net.minecraft.world.level.storage.loot.predicates.LootItemEntityPropertyCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.zawamod.zawa.world.item.ZawaItems;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class LCEntityLoot extends VanillaEntityLoot {
+    @Override
+    public void generate() {
+        add(LCEntities.BANDED_PENGUIN.get(), LootTable.lootTable()
+                .withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F)).add(addItemWithLooting(Items.FEATHER, 1))));
+        add(LCEntities.BOX_TURTLE.get(), LootTable.lootTable()
+                .withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F)).add(addItemWithLooting(Items.BONE, 1))));
+        add(LCEntities.BULLFROG.get(), createCookedItemTable(ZawaItems.SMALL_MEAT.get(), 1));
+        add(LCEntities.CAPYBARA.get(), createCookedItemTable(ZawaItems.SMALL_MEAT.get(), 2)
+                .withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F)).add(addItemWithLooting(Items.BONE, 1))));
+        add(LCEntities.DART_FROG.get(), LootTable.lootTable());
+        add(LCEntities.DWARF_CROCODILE.get(), createCookedItemTable(ZawaItems.SMALL_MEAT.get(), 2)
+                .withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F)).add(addItemWithLooting(Items.LEATHER, 1))));
+        add(LCEntities.GUINEA_PIG.get(), createCookedItemTable(ZawaItems.SMALL_MEAT.get(), 1));
+        add(LCEntities.MYSTERY_SNAIL.get(), createSingleItemTable(LCItems.MYSTERY_SNAIL.get()));
+        add(LCEntities.OPOSSUM.get(), createCookedItemTable(ZawaItems.SMALL_MEAT.get(), 1));
+        add(LCEntities.PACMAN_FROG.get(), createCookedItemTable(ZawaItems.SMALL_MEAT.get(), 1));
+        add(LCEntities.POND_SLIDER.get(), LootTable.lootTable());
+        add(LCEntities.PUMPKIN_TOADLET.get(), LootTable.lootTable());
+        add(LCEntities.RACCOON.get(), createCookedItemTable(ZawaItems.SMALL_MEAT.get(), 1));
+        add(LCEntities.SKUNK.get(), createCookedItemTable(ZawaItems.SMALL_MEAT.get(), 1));
+        add(LCEntities.SMALL_CLAWED_OTTER.get(), createCookedItemTable(ZawaItems.SMALL_MEAT.get(), 1));
+        add(LCEntities.TOMATO_FROG.get(), createCookedItemTable(ZawaItems.SMALL_MEAT.get(), 1));
+        add(LCEntities.TREE_MONITOR.get(), createCookedItemTable(ZawaItems.SMALL_MEAT.get(), 1)
+                .withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F)).add(addItemWithLooting(Items.BONE, 1))));
+        add(LCEntities.TREE_SQUIRREL.get(), createCookedItemTable(ZawaItems.SMALL_MEAT.get(), 1));
+        add(LCEntities.TUFTED_DEER.get(), createCookedItemTable(ZawaItems.SMALL_MEAT.get(), 2)
+                .withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F)).add(addItemWithLooting(Items.BONE, 1))));
+    }
+
+    public static LootTable.Builder createSingleItemTable(ItemLike item) {
+        return LootTable.lootTable()
+                .withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F)).add(LootItem.lootTableItem(item)));
+    }
+
+    public static LootTable.Builder createCookedItemTable(ItemLike item, int maxCount) {
+        return LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F)).add(LootItem.lootTableItem(item)
+                .apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, maxCount)))
+                .apply(SmeltItemFunction.smelted().when(LootItemEntityPropertyCondition.hasProperties(LootContext.EntityTarget.THIS, ENTITY_ON_FIRE)))
+                .apply(LootingEnchantFunction.lootingMultiplier(UniformGenerator.between(0.0F, 1.0F)))));
+    }
+
+    public LootPoolEntryContainer.Builder<?> addItemWithLooting(ItemLike item, int maxCount) {
+        return LootItem.lootTableItem(item)
+                .apply(SetItemCountFunction.setCount(UniformGenerator.between(0.0F, maxCount)))
+                .apply(LootingEnchantFunction.lootingMultiplier(UniformGenerator.between(0.0F, 1.0F)));
+    }
+
+    @Override
+    protected Stream<EntityType<?>> getKnownEntityTypes() {
+        return ForgeRegistries.ENTITY_TYPES.getEntries().stream()
+                .filter(e -> e.getKey().location().getNamespace().equals(LilCritters.MOD_ID))
+                .map(Map.Entry::getValue);
+    }
+}
