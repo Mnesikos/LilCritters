33642dc45f3c4e97732e6a4f7554082f876fe006
Mnesikos <mnesikos@gmail.com>
2026-07-04 23:32:07 -0400

    lol fix dwarf croc doc variant baby texture

--- src/main/java/com/github/mnesikos/lilcritters/client/render/entity/DwarfCrocodileRenderer.java
+++ src/main/java/com/github/mnesikos/lilcritters/client/render/entity/DwarfCrocodileRenderer.java
@@ -8,6 +8,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import org.zawamod.zawa.client.renderer.entity.ZawaMobRenderer;
+import org.zawamod.zawa.resources.EntityStatsManager;
 
 public class DwarfCrocodileRenderer extends ZawaMobRenderer<DwarfCrocodileEntity, DwarfCrocodileModel> {
     public DwarfCrocodileRenderer(EntityRendererProvider.Context context) {
@@ -18,6 +19,14 @@ public class DwarfCrocodileRenderer extends ZawaMobRenderer<DwarfCrocodileEntity
     protected void scale(DwarfCrocodileEntity entity, PoseStack matrixStack, float partialTickTime) {
         if (entity.isBaby()) matrixStack.scale(0.5F, 0.5F, 0.5F);
         super.scale(entity, matrixStack, partialTickTime);
+    }
+
+    @Override
+    public ResourceLocation getBabyTexture(DwarfCrocodileEntity entity, int variant) {
+        if (variant >= entity.getWildVariants()) {
+            String variantName = EntityStatsManager.INSTANCE.getStats(entity).getCaptiveVariantsList().get(variant - entity.getWildVariants());
+            return new ResourceLocation(LilCritters.MOD_ID, "textures/entity/dwarf_crocodile/dwarf_crocodile_" + variantName + ".png");
+        } else return this.babyTexture != null ? this.babyTexture : this.babyTextures[variant];
     }
 
     @Override
